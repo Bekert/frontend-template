c589e704c9e17c1bee537a25eb90aa75a38b834a
Bekert <moroz.bogdan@protonmail.com>
2022-07-09 10:34:58 +0300

    feat: Added react-bootstrap to project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types'
 
 import Layout from '../components/layout'
+import { SSRProvider } from 'react-bootstrap'
+
 import '../styles/global.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<SSRProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SSRProvider>
 	)
 }
 
