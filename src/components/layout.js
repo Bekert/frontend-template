a3e238006034eaca6e50afc0086866de2af4e5f8
Bekert <moroz.bogdan@protonmail.com>
2022-07-09 11:12:21 +0300

    feat: Added basic info for SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,18 @@
+import Head from 'next/head'
 import PropTypes from 'prop-types'
 import { SSRProvider } from 'react-bootstrap'
 
 function Layout({ children }) {
 	return (
-		<SSRProvider>
-			<main>{children}</main>
-		</SSRProvider>
+		<>
+			<Head>
+				<title>Next JS Template</title>
+				<meta name='description' content='Description of simple Next JS template' />
+			</Head>
+			<SSRProvider>
+				<main>{children}</main>
+			</SSRProvider>
+		</>
 	)
 }
 
